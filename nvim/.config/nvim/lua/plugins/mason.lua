08835cb48840678114dfeebbde0055b4630ea306
FauricioCespedes <fauriciocespedes@gmail.com>
2023-09-09 14:05:53 -0600

    Update nvim

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+  "williamboman/mason.nvim",
+  ensure_installed = {
+    "biome",
+    "css-lsp",
+    "emmet-language-server",
+    "eslint-lsp",
+    "js-debug-adapter",
+    "json-lsp",
+    "lua-language-server",
+    "node-debug2-adapter",
+    "php-debug-adapter",
+    "prettier",
+    "shfmt",
+    "stylua",
+    "typescript-language-server",
+  },
+}
